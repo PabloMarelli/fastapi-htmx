fe1f4c9016e18a7296de283102314e610d7d9e7c
Pablo Marelli <pablo.marelli.kenny@gmail.com>
2023-09-17 19:00:52 -0300

    Building app.

---
+++ backend.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI, Query
+from fastapi.responses import HTMLResponse
+
+
+from time import sleep
+from random import randint
+from typing import Optional
+
+import json
+import uvicorn
+
+
